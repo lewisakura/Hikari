32fbe0219b13d8fd2467ef182d520453143cd0df
Lewis Crichton <lewistehminerz@outlook.com>
2020-01-24 17:13:13 +0000

    make hikari#run be suspend as well for future

--- src/main/kotlin/hikari/Hikari.kt
+++ src/main/kotlin/hikari/Hikari.kt
@@ -1,19 +1,13 @@
 package hikari
 
-import hikari.annotations.EventHandler
-import hikari.events.Event
 import hikari.events.EventBus
-import kotlin.reflect.KCallable
-import kotlin.reflect.KClass
-import kotlin.reflect.full.findAnnotation
-import kotlin.reflect.full.hasAnnotation
 
 class Hikari {
     private var intents = 0
 
     val eventBus = EventBus()
 
-    fun ready() {
+    suspend fun ready() {
         // get intents integer ready
         eventBus.eventHandlers.keys.map { it.intents }.forEach { it.forEach { int -> intents += int.id } }
 
